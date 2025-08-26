d97eeb0224f1217199b5aa95104498ba690b4405
hrishin <hrishishinde.p@gmail.com>
2025-08-26 21:37:24 +0100

    fix headers compilation

--- asm_types_workaround.h
+++ asm_types_workaround.h
@@ -3,28 +3,27 @@
 
 // Workaround for missing asm/types.h
 // This provides the basic types that the kernel headers expect
-
-#include <stdint.h>
+// Using direct type definitions instead of stdint.h for BPF compatibility
 
 // Define the types that are typically in asm/types.h
-typedef uint8_t __u8;
-typedef uint16_t __u16;
-typedef uint32_t __u32;
-typedef uint64_t __u64;
+typedef unsigned char __u8;
+typedef unsigned short __u16;
+typedef unsigned int __u32;
+typedef unsigned long long __u64;
 
-typedef int8_t __s8;
-typedef int16_t __s16;
-typedef int32_t __s32;
-typedef int64_t __s64;
+typedef signed char __s8;
+typedef signed short __s16;
+typedef signed int __s32;
+typedef signed long long __s64;
 
 // Network byte order types
-typedef uint32_t __be32;
-typedef uint16_t __be16;
-typedef uint32_t __le32;
-typedef uint16_t __le16;
+typedef __u32 __be32;
+typedef __u16 __be16;
+typedef __u32 __le32;
+typedef __u16 __le16;
 
 // Additional types that might be needed
-typedef uint64_t __be64;
-typedef uint64_t __le64;
+typedef __u64 __be64;
+typedef __u64 __le64;
 
 #endif // ASM_TYPES_WORKAROUND_H
